5b9439e26596b18698a51b92be8532581cc26cbf
Augustus Madden <gusmadden@gmail.com>
2022-04-13 21:04:47 -0400

    Implemented server functionality

--- server.js
+++ server.js
@@ -1,17 +1,84 @@
-/* On the back end, the application should include a `db.json` file that will be used to store and retrieve notes using the `fs` module.
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
-The following HTML routes should be created:
+const noteData = require('./db/db.json')
+const randID = require('./helpers/randID')
+const PORT = 3001;
 
-* `GET /notes` should return the `notes.html` file.
+const app = express();
 
-* `GET *` should return the `index.html` file.
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}));
 
-The following API routes should be created:
+app.use(express.static('public'));
 
-* `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+/* * `GET *` should return the `index.html` file. */
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
- */
+/* `GET /notes` should return the `notes.html` file. */
+app.get('/notes', (req,res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+/* * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON. */
+app.get('/api/notes', (req,res) =>
+    res.status(200).json(noteData)
+);
+
+/* * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you). */
+app.post('/api/notes', (req,res) => {
+    console.info(`${req.method} request received to add a new note`);
+
+    const {title,text} = req.body;
+
+    if (title && text) {
+        // Variable for the new note save
+        const newNote = {
+            title,
+            text,
+            note_id: randID(),
+        };
+
+        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                const parsedNotes = JSON.parse(data);
+
+                parsedNotes.push(newNote);
+
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Note added.')
+                );
+            }
+        })
+        
+        const response = {
+            status: 'sucess',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response)
+    } else {
+        res.status(500).json('An error was encountered when adding your note')
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+});
+
 
 /* GIVEN a note-taking application
 WHEN I open the Note Taker
